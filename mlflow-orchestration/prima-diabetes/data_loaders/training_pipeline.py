5a1050362fa5bbd767e62b31e00bcebd657afb44
bob <davidgillespie1987@gmail.com>
2024-07-12 15:50:34 +0100

    deployment

--- mlflow-orchestration/prima-diabetes/data_loaders/training_pipeline.py
+++ mlflow-orchestration/prima-diabetes/data_loaders/training_pipeline.py
@@ -4,6 +4,9 @@ if "data_loader" not in globals():
     from mage_ai.data_preparation.decorators import data_loader
 if "test" not in globals():
     from mage_ai.data_preparation.decorators import test
+import sklearn
+
+print(sklearn.__version__)
 
 
 @data_loader
